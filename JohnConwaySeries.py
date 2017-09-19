5c510670b6d1ec85777145aa5459945c45382ab9
Fyang <lixleon@foxmail.com>
2017-09-19 11:05:34 +0800

    John Conway Series

---
+++ JohnConwaySeries.py
@@ -0,0 +1,28 @@
+def JCSeries(howmany):
+    if not isinstance(howmany, int):
+        raise Exception('An integer required! ')
+    result = '1'
+    ret = [result, ]
+    for n in range(howmany):
+        strn = '{}'.format(result)
+        result = ''
+        counter = 1
+        number = None
+        for each in strn:
+            if not number:
+                number = each
+                continue
+            if number == each:
+                counter += 1
+            else:
+                result = '{}{}{}'.format(result, counter, number)
+                number = each
+                counter = 1
+        result = '{}{}{}'.format(result, counter, number)
+        ret.append(result)
+    return ret
+
+if __name__ == '__main__':
+    hm = input('Input the size of John Conway Series that you want >> ')
+    for each in JCSeries(int(hm)):
+        print(each)
